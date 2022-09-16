14d0454c7bb6de9700ac0d8162315ef75e736e65
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2022-09-16 16:37:05 -0400

    __main__ now supports small cmdline args

--- src/nx3d/__main__.py
+++ src/nx3d/__main__.py
@@ -1,4 +1,20 @@
+import sys
+
 import nx3d
 
 if __name__ == "__main__":
-    nx3d.demo()
+    if len(sys.argv) > 1:
+        debug = "debug" in sys.argv
+        autolabel = "autolabel" in sys.argv
+        verbose = "verbose" in sys.argv
+        plot_axes = "plot_axes" in sys.argv
+        cam_mouse_control = "cam_mouse_control" in sys.argv
+    else:
+        debug = False
+    nx3d.demo(
+        debug,
+        autolabel=autolabel,
+        verbose=verbose,
+        plot_axes=plot_axes,
+        cam_mouse_control=cam_mouse_control,
+    )
