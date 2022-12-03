27338a8cd1ae27b750315e7e71a1716ca2036a37
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2022-12-03 15:49:14 -0500

    mesh can make bent cyl and bent arrow, but mul and dir demos aren't working yet; wip

--- src/nx3d/mesh.py
+++ src/nx3d/mesh.py
@@ -12,8 +12,8 @@ from panda3d.core import (
 )
 from pyvista import PolyDataFilters as F
 from pyvista.utilities.geometric_objects import (
-    Arrow,
     CircularArc,
+    Cone,
     Cylinder,
     PlatonicSolid,
 )
@@ -93,81 +93,63 @@ def faces(ms):
     return faces
 
 
-def remove_face(verts, mesh):
-    for fc in faces(mesh):
-        print(face)
-        print(fc)
-        return
-
-
-# def glue(c1, c2) -> pv.PolyData:
-#     """ glue the meshes together along some faces """
-#     tf1 = faces(c1)[-2]  # top face of a pv.Cylinder is the penultimate
-#     bf2 = faces(c2)[-1]  # bottom face of a pv.Cylinder is the last
-#     nfvs = np.empty(c1.points[tf1].shape)
-#     nfvs[:, :2] = c2.points[tf1, :2]
-#     nfvs[:, 2] = np.average((c1.points[tf1, 2] + c2.points[bf2, 2]) / 2.)  # has the glued vaces verts
-#     print(tf1)
-#     print(c1.faces)
-#     return
-#     # vertices = np.array([[0, 0, 0], [1, 0, 0], [1, 0.5, 0], [0, 0.5, 0]])
-#     # mesh = pyvista.PolyData(vertices)
-#     # faces = np.hstack([[3, 0, 1, 2], [3, 0, 3, 2]])
-#     # mesh = pyvista.PolyData(vertices, faces)
-
-
-def bent_cylinder(nsegments=3, radius=0.08):
+def bent_cylinder(nsegments=8, nsides=8, radius=0.08, with_arc=False):
+    ca = CircularArc([0, 0, 0], [0, 0, 1], [0, 0, 0.5], resolution=nsegments)
     try:
-        return CircularArc(
-            [0, 0, 0], [0, 0, 1], [0, 0, 0.5], resolution=nsegments
-        ).tube(radius=radius)
+        ct = ca.tube(radius=radius, n_sides=nsides)
     except ValueError as e:
         raise ValueError(
             "Try adjusting nsegments, this may be a numerical error in CircularArc"
         ) from e
+    return (ct, ca) if with_arc else ct
 
 
-if __name__ == "__main__":
-    ms = bent_cylinder(8)
-    if 1:
-        pl = pv.Plotter()
-        pl.add_mesh(ms)
-        pl.show_axes()
-        pl.show(full_screen=False)
+def bent_arrow(nsegments=8.0, nsides=8, radius=0.16, tip_length=0.4, **kwargs):
+    tube, arc = bent_cylinder(nsegments, nsides, radius, **kwargs, with_arc=True)
+    start, end = arc.points[-2], arc.points[-4]
+    direction = start - end
+    center = (start + end) / 2.0
+    cone = Cone(
+        center=center,
+        direction=direction,
+        height=tip_length,
+        radius=radius * (1 + tip_length),
+        capping=True,
+        angle=None,
+        resolution=nsides,
+    )
+    stem = F.boolean_difference(tube.triangulate(), cone.triangulate()).connectivity(
+        largest=True
+    )
+    return stem + cone
 
 
-def bent_arrow(nsegments=3, **kwargs):
-    ...
-
-
-def make_edge(kind="g", height=1, radius=0.4, nsides=9) -> pv.PolyData:
+def make_edge(kind="g", nsegments=8, nsides=8, radius=0.4) -> pv.PolyData:
     allowedkinds = ["g", "m", "d", "md"]
     if kind not in allowedkinds:
         raise ValueError(f"unsupported edge kind {kind}; must be one of {allowedkinds}")
     if kind == "g":
         ms = Cylinder(
-            center=[0, 0, height / 2],
+            center=[0, 0, 0.5],
             direction=[0, 0, 1],
-            height=height,
+            height=1.0,
             radius=radius,
             resolution=nsides,
         )
+        # ms = bent_cylinder(1, nsides, radius)
     elif "d" in kind:
-        ms = bent_arrow(
-            direction=[0, 0, 1],
-            tip_length=0.4,
-            tip_radius=radius * 2,
-            tip_resolution=nsides,
-            shaft_radius=radius,
-            shaft_resolution=nsides,
-            scale=height,
-        )
+        ms = bent_arrow(nsegments, nsides, radius, 1.9)
     else:
-        ms = bent_cylinder(
-            center=[0, 0, height / 2],
-            direction=[0, 0, 1],
-            height=height,
-            radius=radius,
-            resolution=nsides,
-        )
+        ms = bent_cylinder(nsegments, nsides, radius)
+    assert ms is not None
     return ms
+
+
+if __name__ == "__main__":
+    print("--- start demo")
+    ms = bent_arrow(8, tip_length=0.3)
+    pl = pv.Plotter()
+    pl.add_mesh(ms)
+    pl.show_axes()
+    pl.show(full_screen=False)
+    print("--- end demo")
